7f132b6a23dee0c6c4792d17d6ed465304964ce0
manipenumajji <manipenumajji0@gmail.com>
2026-06-23 16:14:18 +0530

    added histogram, win/loss chart,equity curve and trade statistics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import matplotlib.pyplot as plt
 #initilizing database function
 def initialize_database():
 
@@ -242,14 +243,188 @@ PnL     : {trade[6]}
     print("Trade Deleted Successfully!")
 
     connection.close()
+# trade_statistics function    
+def trade_statistics():
 
+    connection = sqlite3.connect("trades.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT pnl FROM trades")
+    trades = cursor.fetchall()
+
+    if not trades:
+        print("No trades found!")
+        connection.close()
+        return
+
+    total_trades = len(trades)
+
+    total_pnl = 0
+    winning_trades = 0
+    losing_trades = 0
+
+    for trade in trades:
+
+        pnl = trade[0]
+
+        total_pnl += pnl
+
+        if pnl > 0:
+            winning_trades += 1
+
+        elif pnl < 0:
+            losing_trades += 1
+
+    average_pnl = round(total_pnl / total_trades, 2)
+
+    win_rate = round(
+        (winning_trades / total_trades) * 100,
+        2
+    )
+
+    print(f"""
+Trade Statistics
+----------------
+Total Trades   : {total_trades}
+Winning Trades : {winning_trades}
+Losing Trades  : {losing_trades}
+Total PnL      : {round(total_pnl,2)}
+Average PnL    : {average_pnl}
+Win Rate       : {win_rate}%
+""")
+
+    connection.close()
+#equity curve function    
+def plot_equity_curve():
+
+    connection = sqlite3.connect("trades.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+    SELECT pnl
+    FROM trades
+    ORDER BY id
+    """)
+
+    trades = cursor.fetchall()
+
+    if not trades:
+        print("No trades found!")
+        connection.close()
+        return
+
+    cumulative_pnl = []
+
+    running_total = 0
+
+    for trade in trades:
+
+        running_total += trade[0]
+
+        cumulative_pnl.append(running_total)
+
+    connection.close()
+
+    plt.figure(figsize=(8,5))
+
+    plt.plot(cumulative_pnl, marker="o")
+
+    plt.title("Equity Curve")
+
+    plt.xlabel("Trade Number")
+
+    plt.ylabel("Cumulative PnL")
+
+    plt.grid(True)
+
+    plt.show()
+#win or loss chart function    
+def plot_win_loss_chart():
+
+    connection = sqlite3.connect("trades.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT pnl FROM trades")
+
+    trades = cursor.fetchall()
+
+    if not trades:
+        print("No trades found!")
+        connection.close()
+        return
+
+    wins = 0
+    losses = 0
+
+    for trade in trades:
+
+        pnl = trade[0]
+
+        if pnl > 0:
+            wins += 1
+
+        elif pnl < 0:
+            losses += 1
+
+    connection.close()
+
+    plt.figure(figsize=(6, 6))
+
+    plt.pie(
+        [wins, losses],
+        labels=["Wins", "Losses"],
+        autopct="%1.1f%%"
+    )
+
+    plt.title("Win/Loss Distribution")
+
+    plt.show()  
+#histogram the deadly combo    
+def plot_pnl_histogram():
+
+    connection = sqlite3.connect("trades.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT pnl FROM trades")
+
+    trades = cursor.fetchall()
+
+    if not trades:
+        print("No trades found!")
+        connection.close()
+        return
+
+    pnl_values = []
+
+    for trade in trades:
+        pnl_values.append(trade[0])
+
+    connection.close()
+
+    plt.figure(figsize=(8,5))
+
+    plt.hist(pnl_values, bins=10)
+
+    plt.title("PnL Distribution")
+
+    plt.xlabel("PnL")
+
+    plt.ylabel("Number of Trades")
+
+    plt.grid(True)
+
+    plt.show()      
 while True:
     print("\n Trading journal\n"
     "1.add trade\n"
     "2.view trades\n"
     "3.update trade\n"
     "4.delete trade\n" \
-    "5.exit")
+    "5.trade statistics\n" \
+    "6.equity curve\n" \
+    "7.win/loss chart\n" \
+    "8.pnl histogram\n" \
+    "9.exit")
     try:
         choice=int(input("enter your choice: "))
     except ValueError:
@@ -265,7 +440,15 @@ while True:
         update_trade()
     elif choice == 4:
         delete_trade()
-    elif choice == 5:
+    elif choice==5:
+        trade_statistics()  
+    elif choice==6:
+        plot_equity_curve()   
+    elif choice == 7:
+        plot_win_loss_chart()
+    elif choice == 8:
+        plot_pnl_histogram()           
+    elif choice == 9:
         print("Good Luck Comeback Stronger!")
         break
         
